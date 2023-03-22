cf18c328967904bedf7cd4e004ef49192bf8ac41
aMeijerr <caa.meijer@gmail.com>
2023-03-22 16:04:53 +0100

    Add new endpoint and change names to be more precise

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,8 +12,8 @@ export class DataController {
   dataset = this.bigQuery.dataset('SB_totals');
   table = this.dataset.table('sb_data_mars');
 
-  @Get('toplist')
-  async getTopList(
+  @Get('producerToplist')
+  async getProducerTopList(
     @Query('category') category: string[] | undefined,
     @Query('minYear') minYear: number,
     @Query('maxYear') maxYear: number,
@@ -36,6 +36,31 @@ export class DataController {
     const [rows] = await this.table.query(toplist_query.toString());
     return rows;
   }
+
+  // @Get('toplist')
+  // async getTopList(
+  //   @Query('category') category: string[] | undefined,
+  //   @Query('minYear') minYear: number,
+  //   @Query('maxYear') maxYear: number,
+  // ) {
+  //   let toplist_query = `SELECT producer_name, SUM(sales) AS total_sales FROM ${this.table.id}`;
+  //   let dateFilter = '';
+  //   if (minYear && maxYear) {
+  //     dateFilter += ` date BETWEEN ${minYear} AND ${maxYear} AND`;
+  //   }
+  //   if (category) {
+  //     dateFilter += ` product_group = '${category}' AND`;
+  //   }
+
+  //   if (dateFilter) {
+  //     dateFilter = ` WHERE ${dateFilter.slice(0, -4)}`;
+  //     toplist_query += `${dateFilter} GROUP BY producer_name ORDER BY total_sales DESC LIMIT 10`;
+  //   } else {
+  //     toplist_query += ` GROUP BY producer_name ORDER BY total_sales DESC LIMIT 10`;
+  //   }
+  //   const [rows] = await this.table.query(toplist_query.toString());
+  //   return rows;
+  // }
 
   @Get('country')
   async getCountries(
